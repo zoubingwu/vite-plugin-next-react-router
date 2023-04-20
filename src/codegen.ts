06be16e9178fa12d020dc9d0b61de83a7cf52dda
zoubingwu <zoubingwu@gmail.com>
2023-04-20 10:35:58 +0800

    fix: path nomarlize

--- src/codegen.ts
+++ src/codegen.ts
@@ -1,3 +1,4 @@
+import { normalizePath } from 'vite';
 import { resolveGlobalLayout } from './resolver';
 import { generateRoutesCode } from './template';
 import { ResolvedOptions, ResolvedRoute } from './types';
@@ -6,7 +7,7 @@ import { stripExt } from './utils';
 export const layoutImport = (options: ResolvedOptions) => {
   const layout = resolveGlobalLayout(options);
   const imports = layout
-    ? `import ${layout.name} from '${stripExt(layout.path)}'`
+    ? `import ${layout.name} from '${normalizePath(stripExt(layout.path))}'`
     : `import { Outlet } from 'react-router-dom';`;
   const element = layout ? layout.name : 'Outlet';
 
@@ -22,7 +23,7 @@ export const routeObjects = (
       return `{
         path: '${route.route}',
         async lazy() {
-          const c = await import('${stripExt(route.path)}');
+          const c = await import('${normalizePath(stripExt(route.path))}');
           return { Component: c.default }
         },
         index: ${route.index ?? false}
