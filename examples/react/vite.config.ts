23aa5adfff40399f9fcf6972a26a9c48ef60f792
zoubingwu <zoubingwu@gmail.com>
2023-04-20 10:12:20 +0800

    fix: update example

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react';
-import { reactRouterPlugin } from 'vite-plugin-next-react-router';
+import Inspect from 'vite-plugin-inspect';
+import withReactRouter from 'vite-plugin-next-react-router';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    reactRouterPlugin({
-      async: true,
-    }),
-  ],
+  plugins: [withReactRouter(), reactRefresh(), Inspect()],
 });
